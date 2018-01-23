acbf11dc178cdd05232c9bd06ae9b942ffbbf11e
Markus Baars <mbaars@web.de>
2018-01-23 15:01:35 +0100

    Creating an out parameter method

--- BattleTanks/Source/BattleTanks/TankPlayerController.h
+++ BattleTanks/Source/BattleTanks/TankPlayerController.h
@@ -24,4 +24,7 @@ public:
 	// Start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
+
+private:
+	bool GetSightRayHitLocation(FVector &OutHitLocation) const;
 };

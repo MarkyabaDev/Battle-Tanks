c29aba4867642d3374b7f3ace41dbccc39a54740
Markus Baars <mbaars@web.de>
2019-03-10 16:45:54 +0100

    HealthBar

--- BattleTanks 4.21 - 2/Source/BattleTanks/Public/Tank.h
+++ BattleTanks 4.21 - 2/Source/BattleTanks/Public/Tank.h
@@ -15,6 +15,9 @@ class BATTLETANKS_API ATank : public APawn
 public:
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+	// Returns currentHealth as Percantage of starting Heath between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
 private:
 	// Sets default values for this pawn's properties
 	ATank();
